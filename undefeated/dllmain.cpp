891534332276f7372c1a589c11b084b7b96109bd
solitude <34527345+solitude70@users.noreply.github.com>
2017-12-15 00:08:52 +1100

    Removed hwid check

--- undefeated/dllmain.cpp
+++ undefeated/dllmain.cpp
@@ -1,41 +1,13 @@
-#include <Windows.h>
-
 #include "stdafx.h"
 
-#define undefeated /* define hwid here */(9999999999)
-
-UCHAR
-szFileSys[255],
-szVolNameBuff[255];
-
-DWORD
-dwMFL,
-dwSysFlags,
-dwSerial;
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD Reason, LPVOID lpReserved)
-{
-	if (Reason == DLL_PROCESS_ATTACH)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
-		GetVolumeInformation("C:\\", (LPTSTR)szVolNameBuff, 255, &dwSerial, &dwMFL, &dwSysFlags, (LPTSTR)szFileSys, 255);
+		CreateThread(nullptr, 0, static_cast<LPTHREAD_START_ROUTINE>(CheatMain), nullptr, 0, nullptr);
 
-		if (dwSerial == undefeated)
-		{
-			MessageBox(0, "valid hwid", "solitude", MB_OK | MB_ICONINFORMATION);
+		INIT::Dll = hModule;
 
-			CreateThread(nullptr, 0, static_cast<LPTHREAD_START_ROUTINE>(CheatMain), nullptr, 0, nullptr);
-
-			INIT::Dll = hModule;
-
-			return TRUE;
-		}
-		else
-		{
-			MessageBox(NULL, "invalid hwid", "solitude", MB_OK);
-
-			ExitProcess(1);
-
-			return FALSE;
-		}
+		return TRUE;
 	}
+	return FALSE;
 }
